73ad49439188c91342d46f63a32230e5b535dc03
Jiaming Tang <jiaming.tang@jetbrains.com>
2025-06-06 14:39:22 +0200

    add python generated sources

--- simpleMultiLanguageTest/python/main/main.py
+++ simpleMultiLanguageTest/python/main/main.py
@@ -3,6 +3,8 @@ from my_lib2.util import bbb
 import os
 import numpy as np
 import requests
+from urban.cities import print_cities
+from artificial.plastics import print_plastics
 if __name__ == "__main__":
     print(requests.__version__)
     print(np.version.version)
