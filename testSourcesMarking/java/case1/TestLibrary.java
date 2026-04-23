b495a925b685947776752274baa61b73dd98e053
Marcin Kocot <marcin.kocot.external@jetbrains.com>
2026-04-23 13:49:33 +0200

    BAZEL-2917: Test sources marking testcase

---
+++ testSourcesMarking/java/case1/TestLibrary.java
@@ -0,0 +1,11 @@
+package case1;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class JavaTest {
+  @Test
+  public void addTest() {
+    Assert.assertEquals(4, 2 + 2);
+  }
+}
---
+++ testSourcesMarking/java/case2/JavaBinary.java
@@ -0,0 +1,7 @@
+package case2;
+
+public class JavaBinary {
+  public static void main(String[] args) {
+    NormalLibrary1 normalLibrary = new NormalLibrary1();
+  }
+}
